78a7ed2d355b40c3931810ec8b2822ef254c1f85
mimica123 <clavicreg@gmail.com>
2021-09-24 17:40:10 -0800

    prediction

--- src/Clara/definitions2.py
+++ src/Clara/definitions2.py
@@ -58,33 +58,35 @@ def categorize(data):
     data = data.cat.codes
     return data
 
-def weight(feature1,feature2, header):
+def weight(data,feature1, feature2, header):
     #################################
     # Creating weight for each value in feature 1 and then assign 
     # them acording to feature 2. We need to know the header of the data
     # Expected value:
-    #      -A dataframe with a feature
-    #      -A dataframe with another feature
+    #      -A dataframe with the values to compare
+    #      -A dataframe with a feature same as the one before
+    #      -the same dataframe with another feature
     #      -The name of each of the features
     #     
     # Return:
     #      -A dataframe with weights
     #
-    # Example:  weight([1,2,3,4,5],[a,b,a,c,b],['num','code'])
+    # Example:  weight(data[a,b,a,c,b],feature1[a,b,c],feature2[1,2,3,4,5],['code','num'])
     #                for a: 1+3=4   1/4, 3/4
     #                for b: 2+5=7   2/7, 5/7
     #                for c: 4       4/4=1
     # Return: [1/4,2/7,3/4,1,5/7]
     #################################
     import pandas as pd
-    data=pd.DataFrame(list(zip(feature1,feature2)), columns=header)
-    group=data[[header[0],header[1]]].groupby(header[0]).sum().reset_index()
-    
-    group['perc']=group[header[1]]/len(data)*100
+    feature=pd.DataFrame(list(zip(feature1,feature2)), columns=header)
+    data=pd.DataFrame(data)
     weight=[]
     for i in data[header[0]]:
-        if not group[group[header[0]]==i]['perc'].values>1:
-            weight.append(group[group[header[0]]==i]['perc'].values[-1])
+        if not feature[feature[header[0]]==i][header[1]].values>1:
+            if len(feature[feature[header[0]]==i][header[1]].values)>0:            
+                weight.append(feature[feature[header[0]]==i][header[1]].values[-1])
+            else:
+                weight.append(0)
         else:
             weight.append(0)
     return weight
@@ -111,38 +113,38 @@ def ols(y,X):
     print('Parameters: ', results.params)
     print('R2: ', results.rsquared)
     
-def linear_regression(X,y):
+def linear_regression(X):
     from sklearn.model_selection import train_test_split
     from sklearn.linear_model import LinearRegression
     from sklearn.metrics import r2_score
-    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,shuffle=True,random_state=0)
+    import pandas as pd
+    #importing data for training
+    df=pd.read_csv('C:/Users/bd/lighthouse-data-notes/Week_5/Day_5/mid-term-project-I/fligths_all_f.csv')
+    features=df[['op_unique_carrier','mkt_carrier_fl_num','origin_airport_id','dest_airport_id','crs_dep_time','crs_arr_time','crs_elapsed_time','distance','year','month','minutes','hours','w_delay_origin','w_delay_destination','w_delay_distance','w_delay_month','w_delay_hour','w_delay_departure','arr_delay']]
+    y_train=features['arr_delay']
+    X_train=features.drop(columns=['arr_delay'])
     model = LinearRegression()
     model.fit(X_train, y_train)
-    y_train_pred = model.predict(X_train)
-    y_test_pred = model.predict(X_test)
+    X_test=X[['op_unique_carrier','mkt_carrier_fl_num','origin_airport_id','dest_airport_id','crs_dep_time','crs_arr_time','crs_elapsed_time','distance','year','month','minutes','hours','w_delay_origin','w_delay_destination','w_delay_distance','w_delay_month','w_delay_hour','w_delay_departure']]
+    X['prediction'] = model.predict(X_test)
 
-    r2_train = r2_score(y_train, y_train_pred)
-    r2_test = r2_score(y_test, y_test_pred)
-
-    print(f'Train R^2:\t{r2_train}\n\
-    Test R^2:\t{r2_test}')
+    return X[['fl_date','mkt_carrier','mkt_carrier_fl_num','origin','dest','prediction']]
     
-def polynomial_regression(X,y):
+    
+def polynomial_regression(X):
     from sklearn.preprocessing import PolynomialFeatures
     from sklearn.model_selection import train_test_split
     from sklearn.metrics import r2_score
-    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,shuffle=True,random_state=0)
+    #importing data for training
+    df=pd.read_csv('C:/Users/bd/lighthouse-data-notes/Week_5/Day_5/mid-term-project-I/fligths_all_f.csv')
+    features=df[['op_unique_carrier','mkt_carrier_fl_num','origin_airport_id','dest_airport_id','crs_dep_time','crs_arr_time','crs_elapsed_time','distance','year','month','minutes','hours','w_delay_origin','w_delay_destination','w_delay_distance','w_delay_month','w_delay_hour','w_delay_departure','arr_delay']]
+    y_train=features['arr_delay']
+    X_train=features.drop(columns=['arr_delay'])
     polynomial = PolynomialFeatures(degree=2)
 
     Xpoly_train = polynomial.fit_transform(X_train)
-    Xpoly_test = polynomial.transform(X_test)
+    polynomial.fit(Xpoly_train, y_train)
+  
+    ypoly_test_pred = polynomial.predict(X)
 
-    print(f'Number of polynomial features: {Xpoly_train.shape[1]}')
-
-    model.fit(Xpoly_train, y_train)
-    ypoly_train_pred = polynomial.predict(Xpoly_train)
-    ypoly_test_pred = polynomial.predict(Xpoly_test)
-
-    r2poly_train = r2_score(y_train, ypoly_train_pred)
-    r2poly_test = r2_score(y_test, ypoly_test_pred)
-    print(f'Train R^2:\t{r2poly_train}\nTest R^2:\t{r2poly_test}')
+    return ypoly_test_pred
